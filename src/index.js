ae3db5cd98026860ef1b54f6b9a43a84964bc0d3
Francisco Flores <fflores.ic.ipn@hotmail.com>
2020-08-15 11:24:56 -0500

    Configuración Webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HolaMundo from './components/HolaMundo';
+import HolaMundo from './components/HolaMundo.jsx';
 
 ReactDOM.render(
     <HolaMundo/>,
